9722138fdd583b5243f6a3017b465cb9968425c7
N8TR0N <n8tr0ncrypto@gmail.com>
2022-02-21 23:54:59 -0500

    commit

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,29 +1,20 @@
 import React from "react";
-import header_pic from "../../assets/2-2.png";
+import header_pic from "../../assets/ninja-trio.png";
 import star from "../../assets/star.png";
 import "./hero.css";
 import { NavLink } from "react-router-dom";
-import {
-  MouseParallaxContainer,
-  MouseParallaxChild,
-} from "react-parallax-mouse";
+import { MouseParallaxContainer, MouseParallaxChild, } from "react-parallax-mouse";
 
 const Hero = () => {
   return (
-    <MouseParallaxContainer className="hero">
+    <MouseParallaxContainer factorX={0.02} factorY={0.04} className="hero">
       <MouseParallaxChild factorX={0.03} factorY={0.05} className="inner1">
-        <img
-          src={header_pic}
-          className="ninja-fade"
-          alt="Welcome To Monkey Ninja Dynasty"
-        />
+        <img src={header_pic} className="ninja-fade" alt="Monkey Ninja" />
       </MouseParallaxChild>
       <div className="inner2">
         <h1>Hi, Ninjas!</h1>
         <h2 style={{ marginTop: "-20px" }}>Welcome To Class</h2>
-        {/*<button onClick={()=> window.open("https://discord.com/invite/Q6xzCsEUFH", "_blank")}>
-                    Join Us On Discord
-                </button>*/}
+        {/*<button onClick={()=> window.open("https://discord.com/invite/Q6xzCsEUFH", "_blank")}>Join Us On Discord</button>*/}
         <h4>
           Okane now available
           <br />
